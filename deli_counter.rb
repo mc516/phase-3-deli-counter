249220d415b5c5608501e7b44e5dd30a1d489a28
Mickey <mickeyc11111@gmail.com>
2022-09-05 10:38:44 -0400

    commit tree 2b9de104439a14b53dd743e8d41e9ebf5789da86

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+
+def line queue
+    if queue.length == 0
+        puts "The line is currently empty."
+    elsif queue.length > 0
+        queue.each.with_index(1) do |name, index|
+            puts "The line is currently:  #{index}. #{name}"
+        end    
+    end
+end
+
+def take_a_number(queue, name)
+    ...queue << name
+    queue.each.with_index(1) do |name, index|
+        puts "Welcome, #{name}. You are number #{index} in line."
+    end
+
+end
